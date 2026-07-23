b48efffb64f6c0ec804b302e9ad7128ebed03406
Arslan Ejaz <arslanrajputseo@gmail.com>
2026-07-24 02:57:55 +0500

    Ars

--- src/config/authors.ts
+++ src/config/authors.ts
@@ -2,6 +2,7 @@ export const lucasAlmeida = {
   name: "Lucas Almeida",
   href: "/autor/lucas-almeida/",
   initials: "LA",
+  image: "/lucas-almeida.webp",
   description: [
     "Lucas Almeida escreve guias informativos para tornar temas trabalhistas mais claros no dia a dia.",
     "Seus textos priorizam fontes oficiais, linguagem direta e exemplos que ajudam na conferência do holerite.",
